d474810b98d45310468940b163c518f279a34807
LuoBoss <dev@pescms.com>
2015-10-22 10:10:24 +0800

    调整注释

--- Core/App.class.php
+++ Core/App.class.php
@@ -1,12 +1,13 @@
 <?php
 
 /**
- * PESCMS run in PHP 5.3+
+ * PESCMS for PHP 5.4+
  *
- * Copyright (c) 2014 PESMCMS (http://www.pesmcs.com)
+ * Copyright (c) 2015 PESCMS (http://www.pescms.com)
  *
  * For the full copyright and license information, please view
  * the file LICENSE.md that was distributed with this source code.
+ * @version 2.5
  */
 
 namespace Core;
--- Core/Model/Model.class.php
+++ Core/Model/Model.class.php
@@ -3,10 +3,11 @@
 /**
  * PESCMS for PHP 5.4+
  *
- * Copyright (c) 2014 PESCMS (http://www.pescms.com)
+ * Copyright (c) 2015 PESCMS (http://www.pescms.com)
  *
  * For the full copyright and license information, please view
  * the file LICENSE.md that was distributed with this source code.
+ * @version 2.5
  */
 
 namespace Core\Model;
