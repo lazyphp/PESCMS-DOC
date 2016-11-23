f7b81b2d60e0be08376718c4f0e3a58be952e451
luoboss <dev@pescms.com>
2016-11-23 10:31:03 +0800

    1.3.8

--- App/Doc/PUT/Setting.class.php
+++ App/Doc/PUT/Setting.class.php
@@ -90,6 +90,7 @@ class Setting extends \Core\Controller\Controller {
                         'option_name' => 'version'
                     ]
                 ]);
+                $version = $iniversion;
             }
         }
         //移除升级的配置文件
