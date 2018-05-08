23488d67fb4ff79cadc1505087df296d76167a27
luoboss <dev@pescms.com>
2018-05-08 22:51:33 +0800

    补充创建子分类时记录当前父类的版本号

--- App/Doc/POST/Tree.php
+++ App/Doc/POST/Tree.php
@@ -10,14 +10,18 @@ class Tree extends Content {
     public function action($jump = FALSE, $commit = FALSE) {
         if(empty($_POST['parent']) && empty($_POST['version']) ){
             $this->error('请填写版本号');
+        }elseif(!empty($_POST['parent'])){
+            $parentTree = \Model\Content::findContent('tree', $_POST['parent'], 'tree_id');
+            if(empty($parentTree)){
+                $this->error('该目录不存在');
+            }
         }
-
 
         parent::action($jump, $commit);
         $treeID = $this->db()->getLastInsert;
         $this->db('tree_version')->insert([
             'tree_id' => $treeID,
-            'tree_version' => $this->p('version')
+            'tree_version' => !empty($parentTree['tree_version']) ? $parentTree['tree_version'] : $this->p('version')
         ]);
 
         $this->db()->commit();
