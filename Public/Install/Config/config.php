31824bafee81bf4a1f149601154450c1130436e8
luoboss <dev@pescms.com>
2022-06-01 14:42:09 +0800

    调整安装界面，添加勾选阅读和同意软件协议的声明。

--- Public/Install/Config/config.php
+++ Public/Install/Config/config.php
@@ -1,14 +1,7 @@
 <?php
  return array(
-'DB_TYPE' => 'mysql',
-'DB_HOST' => 'localhost',
-'DB_NAME' => 'test',
-'DB_USER' => 'root',
-'DB_PWD' => '123456',
-'DB_PORT' => '3306',
 'DB_PREFIX' => 'pes_',
-'PRIVATE_KEY' => '1199738d63',
-'USER_KEY' => '7b641c40ea',
+'USER_KEY' => 'PCINSTALL',
 'APP_GROUP_LIST' => 'Install',
 'DEFAULT_GROUP' => 'Install',
 'SESSION_ID' => 'PESDOCSESSION',
