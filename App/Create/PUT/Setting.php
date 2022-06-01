fbf0ecfac47287d6969e5b1d01b4dab7d548be7c
luoboss <dev@pescms.com>
2022-06-01 16:58:00 +0800

    修正提示栏被遮挡的问题。

--- App/Create/PUT/Setting.php
+++ App/Create/PUT/Setting.php
@@ -116,8 +116,8 @@ class Setting extends \Core\Controller\Controller {
      * 手动更新
      */
     public function mtUpgrade() {
-        $file = $_FILES['zip'];
-        if (pathinfo($file['name'])['extension'] != 'zip') {
+        $file = $_FILES['zip'] ?? NULL;
+        if ( empty($file) || pathinfo($file['name'])['extension'] != 'zip') {
             $this->error('请导入zip的更新补丁');
         }
 
