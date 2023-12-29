ab082cd08aa4471c781515c33732cefcd7a3ff96
luoboss <dev@pescms.com>
2023-12-29 11:14:49 +0800

    添加安装指向检测。

--- Public/Install/Theme/Install/Default/Index/Index_check.php
+++ Public/Install/Theme/Install/Default/Index/Index_check.php
@@ -34,3 +34,32 @@
         </a>
     </div>
 </div>
+
+<div class="am-form-group">
+    <label class="am-u-sm-2 am-text-right am-text-middle">Public目录指向:</label>
+    <div class="am-u-sm-10 am-text-middle">
+        <a href="https://document.pescms.com/article/1/262604572827058176.html#nav-2-H2" class="am-text-<?= $public == false ? 'success' : 'warning' ?>" target="_blank">
+            <i class=" <?= $public == true ? 'am-icon-warning' : 'am-icon-check' ?>"></i> <?= $public == false ? '' : "系统检测到您没有将程序的运行目录指向到Public，您安装软件后可能会遇到路径错误的问题，请点击本提示获取解决方案。" ?>
+        </a>
+    </div>
+</div>
+<?php if($public === true): ?>
+    <script>
+        $(function (){
+            var d = dialog({
+                fixed: true,
+                title: '系统提示',
+                content:'<p>系统检测到您没有将程序的运行目录指向到Public，您安装软件后可能会遇到路径错误的问题。详情请阅读：<a href="https://document.pescms.com/article/1/262604572827058176.html#nav-2-H2" target="_blank">《软件安装 - 注意事项》</a></p>' +
+                    '<p>请根据浏览器地址栏显示的请求地址，来识别是否指向到Public目录。</p>' +
+                    '<p><strong>正确的请求地址：</strong><span class="am-text-success"><?= $_SERVER['HTTP_HOST'] ?><?= str_replace('Public/', '', $_SERVER['SCRIPT_NAME']) ?> <i class="am-icon-check"></i></span></p>' +
+                    '<p><strong>当前浏览器显示错误的请求地址：</strong><span class="am-text-danger"><?= $_SERVER['HTTP_HOST'] ?><?= $_SERVER['SCRIPT_NAME'] ?> <i class="am-icon-close"></i></span></p>' +
+                    '<p>若信息误报，请忽略本提示。</p>',
+                okValue: '我知道了',
+                ok: function () {
+
+                }
+            });
+            d.showModal();
+        })
+    </script>
+<?php endif; ?>
