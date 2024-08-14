52a6736e512af061acef3c4cf2c8ddfa4862a810
luoboss <dev@pescms.com>
2024-08-14 09:42:51 +0800

    优化设置页填写提示。 添加API更新功能。 优化存活统计脚本。

--- App/Create/PUT/Setting.php
+++ App/Create/PUT/Setting.php
@@ -11,18 +11,18 @@ namespace App\Create\PUT;
 
 class Setting extends \Core\Controller\Controller {
 
-    public function index(){
+    public function index() {
         $list = $this->db('option')->where('option_id > 0')->select();
 
-        foreach ($list as $item){
-            if($item['option_required'] == 1){
+        foreach ($list as $item) {
+            if ($item['option_required'] == 1) {
                 $value = $this->isP($item['option_name'], "{$item['name']}为必填项，请填写/选择该项内容后再次提交", false);
-            }else{
+            } else {
                 $value = $this->p($item['option_name'], false);
             }
 
-            if(isset($item['option_type'])){
-                switch ($item['option_type']){
+            if (isset($item['option_type'])) {
+                switch ($item['option_type']) {
                     case 'json':
                         $value = json_encode(explode(',', $value));
                         break;
@@ -32,9 +32,9 @@ class Setting extends \Core\Controller\Controller {
 
             $this->db('option')->where('option_name = :option_name')->update([
                 'noset' => [
-                    'option_name' => $item['option_name']
+                    'option_name' => $item['option_name'],
                 ],
-                'value' => $value
+                'value' => $value,
             ]);
 
         }
@@ -56,7 +56,7 @@ class Setting extends \Core\Controller\Controller {
                 'X-Requested-With: XMLHttpRequest',
                 'Content-Type: application/json; charset=utf-8',
                 'Accept: application/json',
-            ]
+            ],
         ]), true);
 
         if (empty($getPatch)) {
@@ -73,11 +73,11 @@ class Setting extends \Core\Controller\Controller {
             fwrite($download, $getFile);
             fclose($download);
 
-            if(hash_file('sha256', $patchSave) !== $getPatch['data']['patch_sha256'] ){
+            if (hash_file('sha256', $patchSave) !== $getPatch['data']['patch_sha256']) {
                 exit('哈希值不一致');
             }
 
-            (new \Expand\zip()) ->unzip($patchSave);
+            (new \Expand\zip())->unzip($patchSave);
 
             $this->actionini();
 
@@ -86,13 +86,13 @@ class Setting extends \Core\Controller\Controller {
 
             $continueUrl = $this->url(GROUP . '-Setting-atUpgrade', ['method' => 'PUT', 'complete' => 1]);
 
-            if($getPatch['data']['confirm'] == 1){
+            if ($getPatch['data']['confirm'] == 1) {
                 $this->assign('title', '[重要提示]本次更新需要二次确认');
                 $this->assign('continueUrl', $continueUrl);
                 $this->assign('version', $getPatch['data']['new_version']);
                 $this->assign('detail', $getPatch['data']['update_content']);
                 $this->layout('Setting_upgrade_confirm');
-            }else{
+            } else {
                 //继续跳转至自动更新方法
                 $this->success("{$getPatch['data']['new_version']}升级完毕,自动更新程序正在运行,请勿关闭浏览器", $continueUrl, '1');
             }
@@ -105,12 +105,12 @@ class Setting extends \Core\Controller\Controller {
             $this->upgradeStatistics(\Core\Func\CoreFunc::$param['system']['version']);
 
             //获取从旧版到最新版的升级说明
-            $detail = json_decode((new \Expand\cURL())->init(PESCMS_URL . '/patch/detail', ['method' => 'GET','version' => $this->session()->get('oldVersion'),'project' => 3,],
+            $detail = json_decode((new \Expand\cURL())->init(PESCMS_URL . '/patch/detail', ['method' => 'GET', 'version' => $this->session()->get('oldVersion'), 'project' => 3,],
                 [
                     CURLOPT_HTTPHEADER => [
                         'X-Requested-With: XMLHttpRequest',
                         'Accept: application/json',
-                    ]
+                    ],
                 ]), true);
 
             $this->assign('detail', $detail['data']);
@@ -127,7 +127,7 @@ class Setting extends \Core\Controller\Controller {
      */
     public function mtUpgrade() {
         $file = $_FILES['zip'] ?? NULL;
-        if ( empty($file) || pathinfo($file['name'])['extension'] != 'zip') {
+        if (empty($file) || pathinfo($file['name'])['extension'] != 'zip') {
             $this->error('请导入zip的更新补丁');
         }
 
@@ -137,21 +137,21 @@ class Setting extends \Core\Controller\Controller {
                 'X-Requested-With: XMLHttpRequest',
                 'Content-Type: application/json; charset=utf-8',
                 'Accept: application/json',
-            ]
+            ],
         ]), true);
-        if(empty($getPatch)){
+        if (empty($getPatch)) {
             $this->error('连接PESCMS服务器失败!');
         }
 
-        if($getPatch['status'] != 200){
+        if ($getPatch['status'] != 200) {
             $this->error($getPatch['msg']);
         }
 
-        if(hash_file('sha256', $file['tmp_name']) !== $getPatch['data']['patch_sha256']){
+        if (hash_file('sha256', $file['tmp_name']) !== $getPatch['data']['patch_sha256']) {
             $this->error('非官方更新补丁!请访问<a href="https://www.pescms.com" target="_blank">PESCMS</a>获取最新的补丁', 'javascript:history.go(-1)', '10');
         }
 
-        (new \Expand\zip()) ->unzip($file['tmp_name']);
+        (new \Expand\zip())->unzip($file['tmp_name']);
 
         $this->actionini();
 
@@ -163,7 +163,7 @@ class Setting extends \Core\Controller\Controller {
      * 执行数据库更新
      * @return bool|string
      */
-    private function actionini(){
+    private function actionini() {
         $version = \Core\Func\CoreFunc::$param['system']['version'];
 
         $ini = APP_PATH . 'Upgrade/action.ini';
@@ -180,21 +180,21 @@ class Setting extends \Core\Controller\Controller {
                 //更新SQL信息
                 if (!empty($value['sql'])) {
                     foreach ($value['sql'] as $file) {
-                        $sql = file_get_contents(APP_PATH.'/Upgrade/sql/'.$file);
-                        if(!empty($sql)){
+                        $sql = file_get_contents(APP_PATH . '/Upgrade/sql/' . $file);
+                        if (!empty($sql)) {
                             $this->db()->exec($sql);
-                        }else{
+                        } else {
                             //更新SQL文件失败，则记录起来
-                            $this->info[] = "更新SQL文件出错: ".APP_PATH.'/Upgrade/sql/'.$file;
+                            $this->info[] = "更新SQL文件出错: " . APP_PATH . '/Upgrade/sql/' . $file;
                         }
                     }
                 }
 
                 //移除废弃的文件(更名)
-                if(!empty($value['delete'])){
+                if (!empty($value['delete'])) {
                     foreach ($value['delete'] as $file) {
-                        if(rename(APP_PATH.$file, APP_PATH.$file.'_remove') != true){
-                            $this->info[] = "移除文件出错: ".APP_PATH.$file;
+                        if (rename(APP_PATH . $file, APP_PATH . $file . '_remove') != true) {
+                            $this->info[] = "移除文件出错: " . APP_PATH . $file;
                         }
                     }
                 }
@@ -202,8 +202,8 @@ class Setting extends \Core\Controller\Controller {
                 $this->db('option')->where('option_name = :option_name')->update([
                     'value' => $iniversion,
                     'noset' => [
-                        'option_name' => 'version'
-                    ]
+                        'option_name' => 'version',
+                    ],
                 ]);
             }
         }
@@ -219,9 +219,9 @@ class Setting extends \Core\Controller\Controller {
      */
     private function upgradeStatistics($version) {
         (new \Expand\cURL())->init(PESCMS_URL . '/?g=Api&m=Statistics&a=action', [
-            'id' => 2,
-            'type' => 2,
-            'version' => $version
+            'id'      => 2,
+            'type'    => 2,
+            'version' => $version,
         ]);
     }
 
@@ -229,12 +229,31 @@ class Setting extends \Core\Controller\Controller {
      * 标记已读帮助文档提示
      * @return void
      */
-    public function readHelpDoc(){
+    public function readHelpDoc() {
         $this->db('option')->where('option_id = :option_id')->update([
             'noset' => [
-                'option_id' => '-3'
+                'option_id' => '-3',
             ],
-            'value' => 1
+            'value' => 1,
+        ]);
+    }
+
+    /**
+     * 生成API密钥
+     */
+    public function generateSecret() {
+        \Model\Option::updateApiKEY();
+        \Model\Option::updateApiSecret();
+
+        $apiKey = \Model\Option::getOptionValue('api_key');
+        $apiSecret = \Model\Option::getOptionValue('api_secret');
+
+        $this->success([
+            'msg'  => '生成成功',
+            'data' => [
+                'api_key'    => $apiKey,
+                'api_secret' => $apiSecret,
+            ],
         ]);
     }
 
--- Public/Theme/Create/Default/Setting/Setting_index.php
+++ Public/Theme/Create/Default/Setting/Setting_index.php
@@ -6,17 +6,19 @@
 
         <div class="am-tabs" data-am-tabs="{noSwipe: 1}">
             <ul class="am-tabs-nav am-nav am-nav-tabs">
-                <?php foreach(array_keys($option) as $k => $name): ?>
-                    <li class="<?= $k == 0 ? 'am-active' : '' ?>"><a href="#tab_<?=$name?>"><?= $name ?></a></li>
+                <?php foreach (array_keys($option) as $k => $name): ?>
+                    <li class="<?= $k == 0 ? 'am-active' : '' ?>"><a href="#tab_<?= $name ?>"><?= $name ?></a></li>
                 <?php endforeach; ?>
                 <li class="am-active">
-                    <button type="submit" class="am-btn am-btn-primary am-btn-xs am-radius" ><i class="am-icon-save"></i> 保存设置</button>
+                    <button type="submit" class="am-btn am-btn-primary am-btn-xs am-radius"><i class="am-icon-save"></i>
+                        保存设置
+                    </button>
                 </li>
             </ul>
 
             <div class="am-tabs-bd">
                 <?php foreach ($option as $subtitle => $item): ?>
-                    <div class="am-tab-panel am-fade <?= $subtitle == '网站信息' ? 'am-in am-active' : ''?>" id="tab_<?= $subtitle ?>">
+                    <div class="am-tab-panel am-fade <?= $subtitle == '网站信息' ? 'am-in am-active' : '' ?>" id="tab_<?= $subtitle ?>">
 
                         <div class="am-panel am-panel-default">
                             <div class="am-panel-bd">
@@ -32,12 +34,13 @@
                                                     </div>
                                                 <?php endif; ?>
 
-                                                <?php if($value['field_name'] == 'authorize'): ?>
+                                                <?php if ($value['field_name'] == 'authorize'): ?>
                                                     <div class="am-alert">
-                                                        当前域名：<?= empty($_SERVER['HTTP_HOST']) ? '获取当前域名失败' : $_SERVER['HTTP_HOST'] ?>，请确认您的授权码绑定的域名和当前程序显示的域名一致，否则授权校验会失败。
-                                                        <?php if(!empty($value['value'])): ?>
+                                                        当前域名：<?= empty($_SERVER['HTTP_HOST']) ? '获取当前域名失败' : $_SERVER['HTTP_HOST'] ?>
+                                                        ，请确认您的授权码绑定的域名和当前程序显示的域名一致，否则授权校验会失败。
+                                                        <?php if (!empty($value['value'])): ?>
                                                             | 授权校验结果：
-                                                            <?php if($system['is_authorize'] == 'right'): ?>
+                                                            <?php if ($system['is_authorize'] == 'right'): ?>
                                                                 <span><i class="am-icon-check"></i></span>
                                                             <?php else: ?>
                                                                 <span id="authorize_result"><i class="am-icon-spinner am-icon-spin"></i></span>
@@ -45,7 +48,7 @@
 
                                                         <?php endif; ?>
                                                     </div>
-                                                <?php elseif($value['field_name'] == 'max_upload_size'): ?>
+                                                <?php elseif ($value['field_name'] == 'max_upload_size'): ?>
                                                     <div class="am-alert am-alert-success">
                                                         当前PHP.ini配置最大上传容量: <?= ini_get('max_file_uploads') ?>M
                                                     </div>
@@ -56,7 +59,9 @@
                                     </div>
                                 <?php endforeach; ?>
 
-                                <button type="submit" class="am-btn am-btn-default am-radius" ><i class="am-icon-save"></i> 保存设置</button>
+                                <button type="submit" class="am-btn am-btn-default am-radius">
+                                    <i class="am-icon-save"></i> 保存设置
+                                </button>
 
                             </div>
                         </div>
@@ -67,21 +72,71 @@
         </div>
 
 
-
     </form>
 
 </div>
 <script>
-    $(function (){
-        $('#authorize_result').each(function (){
+
+    function generateSecret() {
+        //弹出对话框，确认是否生成新的API KEY和Secret
+        if (!confirm('确定要生成新的API KEY和Secret吗？')) {
+            return false;
+        }
+        $.getJSON('<?= $label->url('Create-Setting-generateSecret', ['t' => time(), 'method' => 'PUT']) ?>&tt=' + Math.random(), function (res) {
+            if (res.status == 200) {
+                $('input[name="api_key"]').val(res.data.api_key)
+                $('input[name="api_secret"]').val(res.data.api_secret)
+            } else {
+                alert(res.msg)
+            }
+        })
+    }
+
+    $(function () {
+
+        $('#authorize_result').each(function () {
             var dom = $(this);
-            $.getJSON('<?= $label->url('Create-Setting-checkAuthorize', ['t' => time()]) ?>&tt='+Math.random(), function (res){
-                if(res.status == 200){
+            $.getJSON('<?= $label->url('Create-Setting-checkAuthorize', ['t' => time()]) ?>&tt=' + Math.random(), function (res) {
+                if (res.status == 200) {
                     dom.html('<i class="am-icon-check"></i>')
-                }else{
-                    dom.html('<i class="am-icon-remove"></i> '+res.msg)
+                } else {
+                    dom.html('<i class="am-icon-remove"></i> ' + res.msg)
                 }
             })
         })
+
+        const form = document.getElementsByClassName('am-form')[0];
+
+        const requiredFields = form.querySelectorAll('[required]')
+
+        function isFieldValid(field) {
+            if (field.type === 'radio') {
+                const radios = form.querySelectorAll(`[name="${field.name}"]`);
+                return Array.from(radios).some(radio => radio.checked);
+            } else if (field.type === 'checkbox') {
+                return field.checked;
+            } else if (field.tagName.toLowerCase() === 'select') {
+                return field.value !== '';
+            } else {
+                return field.checkValidity();
+            }
+        }
+
+        $('.am-form').submit(function () {
+            if ($(this).validator('isFormValid') == false) {
+                for (let i = 0; i < requiredFields.length; i++) {
+                    if (!isFieldValid(requiredFields[i])) {
+                        var index = $(requiredFields[i]).parents('.am-tab-panel').index();
+                        $('.am-tabs').tabs('open', index)
+                        break;
+                    }
+                }
+            }
+        })
+
+        setTimeout(function () {
+            $('input[name="api_secret"]').after('<button type="button" class="am-btn am-btn-primary am-btn-xs am-radius am-margin-top" onclick="generateSecret()"><i class="am-icon-refresh"></i> 生成新的API KEY和Secret</button>')
+        }, 200)
+
     })
 </script>
